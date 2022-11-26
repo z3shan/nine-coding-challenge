8527b30528a74808ee2d7ce7d296a9c3f6242784
zeeshan <zeeshan.mirza@live.com>
2022-11-26 21:25:37 +1100

    added basic middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
-var express = require('express');
+const express = require('express');
 require('dotenv').config();
-var app = express();
+const bodyParser = require('body-parser');
+const xss = require('xss-clean');
+const helmet = require('helmet');
+const cors = require("cors");
+const app = express();
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+app.use(helmet());
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(xss());
+
+app.use(bodyParser.json());
 
 module.exports = app;
