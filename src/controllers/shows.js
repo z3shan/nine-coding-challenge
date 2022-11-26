33ba88358dca5384a9f972dd9ebd6e61e9039896
zeeshan <zeeshan.mirza@live.com>
2022-11-26 23:57:17 +1100

    refactored codebase to add try catch to handle exception cases

--- src/controllers/shows.js
+++ src/controllers/shows.js
@@ -1,31 +1,41 @@
 const showSchema = require("../validations/shows.validation");
 const getShowsList = (req, res) => {
-    const {body} = req;
-    const {error} = showSchema.validate(body);
 
-    if (error) {
+
+    try {
+        const {body} = req;
+        const {error} = showSchema.validate(body);
+
+
+        if (error) {
+            return res.status(400)
+                .send({
+                    "error": "Could not decode request: JSON parsing failed"
+                });
+        }
+
+        const data = [];
+
+
+        for (const show of body.payload) {
+            if (show.drm && show.episodeCount > 1) {
+                data.push({
+                    image: show.image,
+                    slug: show.slug,
+                    title: show.title
+
+                });
+            }
+        }
+
+
+        return res.status(200).send({"response": data});
+    } catch (err) {
         return res.status(400)
             .send({
                 "error": "Could not decode request: JSON parsing failed"
             });
     }
-
-    const data = [];
-
-
-    for (const show of body.payload) {
-        if (show.drm && show.episodeCount > 1) {
-            data.push({
-                image: show.image,
-                slug: show.slug,
-                title: show.title
-
-            });
-        }
-    }
-
-
-    return res.status(200).send({"response": data});
 
 };
 
