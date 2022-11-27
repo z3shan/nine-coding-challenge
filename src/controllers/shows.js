408f2edaba1e3f8d4f37750176ccbf13398bf12b
zeeshan <zeeshan.mirza@live.com>
2022-11-27 15:49:04 +1100

    refactored codebase

--- src/controllers/shows.js
+++ src/controllers/shows.js
@@ -1,5 +1,4 @@
 const showSchema = require("../validations/shows.validation");
-const logger = require("../logger");
 const getShowsList = (req, res) => {
 
     try {
@@ -18,7 +17,6 @@ const getShowsList = (req, res) => {
         }
         return res.status(200).send({"response": data});
     } catch (err) {
-        logger.error(err);
         return res.status(400)
             .send({
                 "error": "Could not decode request: JSON parsing failed"
